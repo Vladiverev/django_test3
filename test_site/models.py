acfb35717330644d81f2c5fdb30338d4abef020e
vladiverev <vladiverev@gmail.com>
2018-10-16 13:11:24 +0300

    2d

--- test_site/models.py
+++ test_site/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Create your models here.
 class Client(models.Model):
     user_name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    passwords = models.CharField(max_length=200)
     
 
     def __str__(self):
--- test_site/views.py
+++ test_site/views.py
@@ -6,7 +6,29 @@ from django.contrib.auth import login, authenticate
 
 # Create your views here.
 
-def sale_list(request):    
-    dw = datawiz.DW(request.user.username, request.user.password)
-    shop = dw.get_products_sale(by="receipts_qty", date_from="2015-11-17", date_to="2015-11-18", show= "name") 
-    return render(request, 'test_site/sale_list.html', {'shop': shop})
+
+def client_info(sale):
+    #sales = []
+    #for key, s in sale.iterrows():
+    #    sales.append(s.turnover)
+    #return sales
+    sales = {}
+    for key, s in sale.items():
+        sales[key] = s
+    return sales
+
+
+def sale_list(request):
+    try: 
+        dw = datawiz.DW(request.user.username, request.user.password)
+        client = dw.get_client_info()
+        sales = {}
+        for sh in ('turnover', "qty", "receipts_qty"):
+            sales[sh] = dw.get_categories_sale(categories = ["sum"],by = sh,
+						    date_from = "2015-11-17", date_to = "2015-11-18",
+						    interval = 'days', show = sh, view_type = 'raw').to_dict('split')
+        return render(request, 'test_site/sale_list.html', {'client': client, 'sales': sales})
+        
+    except Exception:        
+        return render(request, 'test_site/index.html',{})
+
