736e6a9a795fc7197ae2ef94ed63f27de0a00837
vladiverev <vladiverev@gmail.com>
2018-10-16 17:42:20 +0300

    5th

--- test_site/views.py
+++ test_site/views.py
@@ -25,20 +25,22 @@ def sale_list(request):
         sales = dw.get_categories_sale(categories = ["sum"],by = ['turnover', 'qty', 'receipts_qty'],
 						date_from = "2015-11-17", date_to = "2015-11-18",
 						interval = 'days', view_type = 'raw').drop(['name', 'category', 'date'], axis='columns')
-      #  sales['turnover'] = dw.get_categories_sale(categories = ["sum"],by = 'qty',
-						#date_from = "2015-11-17", date_to = "2015-11-18",
-						#interval = 'days', view_type = 'raw')['qty']        
-      #  sales['qty'] = dw.get_categories_sale(categories = ["sum"],by = "receipts_qty",
-						#date_from = "2015-11-17", date_to = "2015-11-18",
-						#interval = 'days', view_type = 'raw')['receipts_qty']
         sales['average_qty'] = sales['turnover'] / sales['qty']
-        sales1  = sales.T.reset_index()
-        sales1['dif_per'] = sales1[1] / sales1[0]
-        sales1['dif_min'] = sales1[1] - sales1[0]
+        sales1  = sales.T.reset_index().rename(columns={0: "2015-11-17", 1: "2015-11-18"})
+        sales1['dif_per'] = sales1["2015-11-18"] / sales1["2015-11-17"]
+        sales1['dif_min'] = sales1["2015-11-18"] - sales1["2015-11-17"]
 
-                
+        product1 =  dw.get_products_sale(by=['turnover', 'qty'], date_from = "2015-11-17", date_to = "2015-11-17",
+						interval = 'days',show="name", view_type = 'raw').drop(['date', 'product'], axis='columns')
+        product2 =  dw.get_products_sale(by=['turnover', 'qty'], date_from = "2015-11-18", date_to = "2015-11-18",
+						interval = 'days',show='name', view_type = 'raw').drop(['date', 'product'], axis='columns')        
+        product1['differ_qty'] = product1['qty'] - product2['qty']
+        product1['differ_turnover'] = product1['turnover'] - product2['turnover']
+        product = product1.drop(['turnover', 'qty'], axis='columns')
         
-        return render(request, 'test_site/sale_list.html', {'client': client, 'sales': sales1})
+        return render(request, 'test_site/sale_list.html', {'client': client, 'sales': sales1.to_html, 
+                                                            'products1': product.sort_values(by='differ_qty').to_html,
+                                                           'products2': product.sort_values(by='differ_qty', ascending=False).to_html})
         
     except Exception:        
         return render(request, 'test_site/index.html',{})
