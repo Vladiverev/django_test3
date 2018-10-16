78e107906a9a6018fb6ac2706e098bec73fa8b04
vladiverev <vladiverev@gmail.com>
2018-10-16 15:31:30 +0300

    3d

--- test_site/views.py
+++ test_site/views.py
@@ -22,12 +22,21 @@ def sale_list(request):
     try: 
         dw = datawiz.DW(request.user.username, request.user.password)
         client = dw.get_client_info()
-        sales = {}
-        for sh in ('turnover', "qty", "receipts_qty"):
-            sales[sh] = dw.get_categories_sale(categories = ["sum"],by = sh,
-						    date_from = "2015-11-17", date_to = "2015-11-18",
-						    interval = 'days', show = sh, view_type = 'raw').to_dict('split')
-        return render(request, 'test_site/sale_list.html', {'client': client, 'sales': sales})
+        sales = dw.get_categories_sale(categories = ["sum"],by = ['turnover', 'qty', 'receipts_qty'],
+						date_from = "2015-11-17", date_to = "2015-11-18",
+						interval = 'days', view_type = 'raw').drop(['name', 'category', 'date'], axis='columns')
+      #  sales['turnover'] = dw.get_categories_sale(categories = ["sum"],by = 'qty',
+						#date_from = "2015-11-17", date_to = "2015-11-18",
+						#interval = 'days', view_type = 'raw')['qty']        
+      #  sales['qty'] = dw.get_categories_sale(categories = ["sum"],by = "receipts_qty",
+						#date_from = "2015-11-17", date_to = "2015-11-18",
+						#interval = 'days', view_type = 'raw')['receipts_qty']
+        sales['average_qty'] = sales['turnover'] / sales['qty']
+        sales1  = sales.T
+
+                
+        
+        return render(request, 'test_site/sale_list.html', {'client': client, 'sales': sales1})
         
     except Exception:        
         return render(request, 'test_site/index.html',{})
